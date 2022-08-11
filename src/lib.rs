2e70b4ce4013fb8f3a85c620c956bdf7a402a0c7
Filip Lazovic <lazovicff@gmail.com>
2022-08-11 19:11:30 +0200

    Using halo2curves

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,54 @@
 // BabyJubJub elliptic curve implementation in Rust.
 // For LICENSE check https://github.com/arnaucube/babyjubjub-rs
 
-extern crate ff;
-use num_bigint::BigInt;
-use ff::*;
 use halo2curves::bn256::Fr;
-
-#[derive(PrimeField)]
-#[PrimeFieldModulus = "21888242871839275222246405745257275088548364400416034343698204186575808495617"]
-#[PrimeFieldGenerator = "7"]
-pub struct Scalar(FrRepr);
-
-#[macro_use]
-extern crate lazy_static;
+use halo2curves::group::ff::Field;
+use num_bigint::BigUint;
+use std::ops::AddAssign;
+use std::ops::MulAssign;
+use std::ops::SubAssign;
 
 // D = 168696
-pub const D_FR: Fr = Fr::from_raw([
-    0x292F8,
-    0x00,
-    0x00,
-    0x00,
+pub const D: Fr = Fr::from_raw([0x292F8, 0x00, 0x00, 0x00]);
+
+// A = 168700
+pub const A: Fr = Fr::from_raw([0x292FC, 0x00, 0x00, 0x00]);
+
+// 5299619240641551281634865583518297030282874472190772894086521144482721001553
+pub const B8_X: Fr = Fr::from_raw([
+    0x2893F3F6BB957051,
+    0x2AB8D8010534E0B6,
+    0x4EACB2E09D6277C1,
+    0xBB77A6AD63E739B,
 ]);
 
-lazy_static! {
-    static ref D: Scalar = Scalar::from_str("168696").unwrap();
-    static ref A: Scalar = Scalar::from_str("168700").unwrap();
-    static ref B8: Point = Point {
-        x: Scalar::from_str(
-            "5299619240641551281634865583518297030282874472190772894086521144482721001553",
-        ).unwrap(),
-        y: Scalar::from_str(
-            "16950150798460657717958625567821834550301663161624707787222815936182638968203",
-        ).unwrap(),
-    };
-}
+// 16950150798460657717958625567821834550301663161624707787222815936182638968203
+pub const B8_Y: Fr = Fr::from_raw([
+    0x4B3C257A872D7D8B,
+    0xFCE0051FB9E13377,
+    0x25572E1CD16BF9ED,
+    0x25797203F7A0B249,
+]);
+
+// pub const B8: Point = Point {}
 
 #[derive(Clone, Debug)]
 pub struct PointProjective {
-    pub x: Scalar,
-    pub y: Scalar,
-    pub z: Scalar,
+    pub x: Fr,
+    pub y: Fr,
+    pub z: Fr,
 }
 
 impl PointProjective {
     pub fn affine(&self) -> Point {
-        if self.z.is_zero() {
+        if bool::from(self.z.is_zero()) {
             return Point {
-                x: Scalar::zero(),
-                y: Scalar::zero(),
+                x: Fr::zero(),
+                y: Fr::zero(),
             };
         }
 
-        let zinv = self.z.inverse().unwrap();
+        let zinv = self.z.invert().unwrap();
         let mut x = self.x;
         x.mul_assign(&zinv);
         let mut y = self.y;
@@ -66,12 +63,12 @@ impl PointProjective {
         let mut a = self.z;
         a.mul_assign(&q.z);
         let mut b = a;
-        b.square();
+        b = b.square();
         let mut c = self.x;
         c.mul_assign(&q.x);
         let mut d = self.y;
         d.mul_assign(&q.y);
-        let mut e = *D;
+        let mut e = D.clone();
         e.mul_assign(&c);
         e.mul_assign(&d);
         let mut f = b;
@@ -89,7 +86,7 @@ impl PointProjective {
         let mut x3 = a;
         x3.mul_assign(&f);
         x3.mul_assign(&aux);
-        let mut ac = *A;
+        let mut ac = A.clone();
         ac.mul_assign(&c);
         let mut dac = d;
         dac.sub_assign(&ac);
@@ -109,8 +106,8 @@ impl PointProjective {
 
 #[derive(Clone, Debug)]
 pub struct Point {
-    pub x: Scalar,
-    pub y: Scalar,
+    pub x: Fr,
+    pub y: Fr,
 }
 
 impl Point {
@@ -118,18 +115,19 @@ impl Point {
         PointProjective {
             x: self.x,
             y: self.y,
-            z: Scalar::one(),
+            z: Fr::one(),
         }
     }
 
-    pub fn mul_scalar(&self, n: &BigInt) -> Point {
+    pub fn mul_scalar(&self, f: &Fr) -> Point {
         let mut r: PointProjective = PointProjective {
-            x: Scalar::zero(),
-            y: Scalar::one(),
-            z: Scalar::one(),
+            x: Fr::zero(),
+            y: Fr::one(),
+            z: Fr::one(),
         };
         let mut exp: PointProjective = self.projective();
-        let (_, b) = n.to_bytes_le();
+        let b = f.to_bytes();
+        let n = BigUint::from_bytes_le(&b);
         for i in 0..n.bits() {
             if test_bit(&b, i) {
                 r = r.add(&exp);
@@ -150,140 +148,255 @@ pub fn test_bit(b: &[u8], i: usize) -> bool {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
-	use num_bigint::{BigInt, ToBigInt};
+    use num_bigint::BigUint;
+    use num_traits::identities::One;
+    use num_traits::ToPrimitive;
+    use num_traits::Zero;
+
+    /// Convert BigUint into a vector of 64-bit limbs.
+    fn biguint_to_real_u64_vec(mut v: BigUint, limbs: usize) -> Vec<u64> {
+        let m = BigUint::one() << 64;
+        let mut ret = vec![];
+
+        while v > BigUint::zero() {
+            let limb: BigUint = &v % &m;
+            ret.push(limb.to_u64().unwrap());
+            v >>= 64;
+        }
+
+        while ret.len() < limbs {
+            ret.push(0);
+        }
+
+        assert!(ret.len() == limbs);
+
+        ret
+    }
 
     #[test]
     fn test_add_same_point() {
+        let p_x = BigUint::from_str(
+            "17777552123799933955779906779655732241715742912184938656739573121738514868268",
+        )
+        .unwrap();
+        let p_y = BigUint::from_str(
+            "2626589144620713026669568689430873010625803728049924121243784502389097019475",
+        )
+        .unwrap();
+        let p_x_limbs = biguint_to_real_u64_vec(p_x, 4);
+        let p_y_limbs = biguint_to_real_u64_vec(p_y, 4);
+
         let p: PointProjective = PointProjective {
-            x: Scalar::from_str(
-                "17777552123799933955779906779655732241715742912184938656739573121738514868268",
-            )
-            .unwrap(),
-            y: Scalar::from_str(
-                "2626589144620713026669568689430873010625803728049924121243784502389097019475",
-            )
-            .unwrap(),
-            z: Scalar::one(),
+            x: Fr::from_raw([p_x_limbs[0], p_x_limbs[1], p_x_limbs[2], p_x_limbs[3]]),
+            y: Fr::from_raw([p_y_limbs[0], p_y_limbs[1], p_y_limbs[2], p_y_limbs[3]]),
+            z: Fr::one(),
         };
+
+        let q_x = BigUint::from_str(
+            "17777552123799933955779906779655732241715742912184938656739573121738514868268",
+        )
+        .unwrap();
+        let q_y = BigUint::from_str(
+            "2626589144620713026669568689430873010625803728049924121243784502389097019475",
+        )
+        .unwrap();
+        let q_x_limbs = biguint_to_real_u64_vec(q_x, 4);
+        let q_y_limbs = biguint_to_real_u64_vec(q_y, 4);
+
         let q: PointProjective = PointProjective {
-            x: Scalar::from_str(
-                "17777552123799933955779906779655732241715742912184938656739573121738514868268",
-            )
-            .unwrap(),
-            y: Scalar::from_str(
-                "2626589144620713026669568689430873010625803728049924121243784502389097019475",
-            )
-            .unwrap(),
-            z: Scalar::one(),
+            x: Fr::from_raw([q_x_limbs[0], q_x_limbs[1], q_x_limbs[2], q_x_limbs[3]]),
+            y: Fr::from_raw([q_y_limbs[0], q_y_limbs[1], q_y_limbs[2], q_y_limbs[3]]),
+            z: Fr::one(),
         };
+
         let res = p.add(&q).affine();
+        let res_x = BigUint::from_str(
+            "6890855772600357754907169075114257697580319025794532037257385534741338397365",
+        )
+        .unwrap();
+        let res_y = BigUint::from_str(
+            "4338620300185947561074059802482547481416142213883829469920100239455078257889",
+        )
+        .unwrap();
+        let res_x_limbs = biguint_to_real_u64_vec(res_x, 4);
+        let res_y_limbs = biguint_to_real_u64_vec(res_y, 4);
+
         assert_eq!(
             res.x,
-            Scalar::from_str(
-                "6890855772600357754907169075114257697580319025794532037257385534741338397365"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res_x_limbs[0],
+                res_x_limbs[1],
+                res_x_limbs[2],
+                res_x_limbs[3]
+            ])
         );
         assert_eq!(
             res.y,
-            Scalar::from_str(
-                "4338620300185947561074059802482547481416142213883829469920100239455078257889"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res_y_limbs[0],
+                res_y_limbs[1],
+                res_y_limbs[2],
+                res_y_limbs[3]
+            ])
         );
     }
+
     #[test]
     fn test_add_different_points() {
+        let p_x = BigUint::from_str(
+            "17777552123799933955779906779655732241715742912184938656739573121738514868268",
+        )
+        .unwrap();
+        let p_y = BigUint::from_str(
+            "2626589144620713026669568689430873010625803728049924121243784502389097019475",
+        )
+        .unwrap();
+        let p_x_limbs = biguint_to_real_u64_vec(p_x, 4);
+        let p_y_limbs = biguint_to_real_u64_vec(p_y, 4);
+
         let p: PointProjective = PointProjective {
-            x: Scalar::from_str(
-                "17777552123799933955779906779655732241715742912184938656739573121738514868268",
-            )
-            .unwrap(),
-            y: Scalar::from_str(
-                "2626589144620713026669568689430873010625803728049924121243784502389097019475",
-            )
-            .unwrap(),
-            z: Scalar::one(),
+            x: Fr::from_raw([p_x_limbs[0], p_x_limbs[1], p_x_limbs[2], p_x_limbs[3]]),
+            y: Fr::from_raw([p_y_limbs[0], p_y_limbs[1], p_y_limbs[2], p_y_limbs[3]]),
+            z: Fr::one(),
         };
+
+        let q_x = BigUint::from_str(
+            "16540640123574156134436876038791482806971768689494387082833631921987005038935",
+        )
+        .unwrap();
+        let q_y = BigUint::from_str(
+            "20819045374670962167435360035096875258406992893633759881276124905556507972311",
+        )
+        .unwrap();
+        let q_x_limbs = biguint_to_real_u64_vec(q_x, 4);
+        let q_y_limbs = biguint_to_real_u64_vec(q_y, 4);
+
         let q: PointProjective = PointProjective {
-            x: Scalar::from_str(
-                "16540640123574156134436876038791482806971768689494387082833631921987005038935",
-            )
-            .unwrap(),
-            y: Scalar::from_str(
-                "20819045374670962167435360035096875258406992893633759881276124905556507972311",
-            )
-            .unwrap(),
-            z: Scalar::one(),
+            x: Fr::from_raw([q_x_limbs[0], q_x_limbs[1], q_x_limbs[2], q_x_limbs[3]]),
+            y: Fr::from_raw([q_y_limbs[0], q_y_limbs[1], q_y_limbs[2], q_y_limbs[3]]),
+            z: Fr::one(),
         };
+
         let res = p.add(&q).affine();
+        let res_x = BigUint::from_str(
+            "7916061937171219682591368294088513039687205273691143098332585753343424131937",
+        )
+        .unwrap();
+        let res_y = BigUint::from_str(
+            "14035240266687799601661095864649209771790948434046947201833777492504781204499",
+        )
+        .unwrap();
+        let res_x_limbs = biguint_to_real_u64_vec(res_x, 4);
+        let res_y_limbs = biguint_to_real_u64_vec(res_y, 4);
         assert_eq!(
             res.x,
-            Scalar::from_str(
-                "7916061937171219682591368294088513039687205273691143098332585753343424131937"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res_x_limbs[0],
+                res_x_limbs[1],
+                res_x_limbs[2],
+                res_x_limbs[3]
+            ])
         );
         assert_eq!(
             res.y,
-            Scalar::from_str(
-                "14035240266687799601661095864649209771790948434046947201833777492504781204499"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res_y_limbs[0],
+                res_y_limbs[1],
+                res_y_limbs[2],
+                res_y_limbs[3]
+            ])
         );
     }
 
     #[test]
     fn test_mul_scalar() {
+        let p_x = BigUint::from_str(
+            "17777552123799933955779906779655732241715742912184938656739573121738514868268",
+        )
+        .unwrap();
+        let p_y = BigUint::from_str(
+            "2626589144620713026669568689430873010625803728049924121243784502389097019475",
+        )
+        .unwrap();
+        let p_x_limbs = biguint_to_real_u64_vec(p_x, 4);
+        let p_y_limbs = biguint_to_real_u64_vec(p_y, 4);
         let p: Point = Point {
-            x: Scalar::from_str(
-                "17777552123799933955779906779655732241715742912184938656739573121738514868268",
-            )
-            .unwrap(),
-            y: Scalar::from_str(
-                "2626589144620713026669568689430873010625803728049924121243784502389097019475",
-            )
-            .unwrap(),
+            x: Fr::from_raw([p_x_limbs[0], p_x_limbs[1], p_x_limbs[2], p_x_limbs[3]]),
+            y: Fr::from_raw([p_y_limbs[0], p_y_limbs[1], p_y_limbs[2], p_y_limbs[3]]),
         };
-        let res_m = p.mul_scalar(&3.to_bigint().unwrap());
+        let res_m = p.mul_scalar(&Fr::from(3));
         let res_a = p.projective().add(&p.projective());
         let res_a = res_a.add(&p.projective()).affine();
         assert_eq!(res_m.x, res_a.x);
+
+        let res_m_x = BigUint::from_str(
+            "19372461775513343691590086534037741906533799473648040012278229434133483800898",
+        )
+        .unwrap();
+        let res_m_y = BigUint::from_str(
+            "9458658722007214007257525444427903161243386465067105737478306991484593958249",
+        )
+        .unwrap();
+        let res_m_x_limbs = biguint_to_real_u64_vec(res_m_x, 4);
+        let res_m_y_limbs = biguint_to_real_u64_vec(res_m_y, 4);
+
         assert_eq!(
             res_m.x,
-            Scalar::from_str(
-                "19372461775513343691590086534037741906533799473648040012278229434133483800898"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res_m_x_limbs[0],
+                res_m_x_limbs[1],
+                res_m_x_limbs[2],
+                res_m_x_limbs[3]
+            ])
         );
         assert_eq!(
             res_m.y,
-            Scalar::from_str(
-                "9458658722007214007257525444427903161243386465067105737478306991484593958249"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res_m_y_limbs[0],
+                res_m_y_limbs[1],
+                res_m_y_limbs[2],
+                res_m_y_limbs[3]
+            ])
         );
 
-        let n = BigInt::parse_bytes(
-            b"14035240266687799601661095864649209771790948434046947201833777492504781204499",
-            10,
+        let n = BigUint::from_str(
+            "14035240266687799601661095864649209771790948434046947201833777492504781204499",
         )
         .unwrap();
-        let res2 = p.mul_scalar(&n);
+        let n_limbs = biguint_to_real_u64_vec(n, 4);
+        let n_f = Fr::from_raw([n_limbs[0], n_limbs[1], n_limbs[2], n_limbs[3]]);
+        let res2 = p.mul_scalar(&n_f);
+
+        let res2_x = BigUint::from_str(
+            "17070357974431721403481313912716834497662307308519659060910483826664480189605",
+        )
+        .unwrap();
+        let res2_y = BigUint::from_str(
+            "4014745322800118607127020275658861516666525056516280575712425373174125159339",
+        )
+        .unwrap();
+        let res2_x_limbs = biguint_to_real_u64_vec(res2_x, 4);
+        let res2_y_limbs = biguint_to_real_u64_vec(res2_y, 4);
         assert_eq!(
             res2.x,
-            Scalar::from_str(
-                "17070357974431721403481313912716834497662307308519659060910483826664480189605"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res2_x_limbs[0],
+                res2_x_limbs[1],
+                res2_x_limbs[2],
+                res2_x_limbs[3]
+            ])
         );
         assert_eq!(
             res2.y,
-            Scalar::from_str(
-                "4014745322800118607127020275658861516666525056516280575712425373174125159339"
-            )
-            .unwrap()
+            Fr::from_raw([
+                res2_y_limbs[0],
+                res2_y_limbs[1],
+                res2_y_limbs[2],
+                res2_y_limbs[3]
+            ])
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,34 @@ use std::str::FromStr;
 
 use num_bigint::BigUint;
 use num_traits::identities::One;
-use num_traits::Zero;
 use num_traits::ToPrimitive;
+use num_traits::Zero;
 
 // TEST VECTORS
 /// 0xe0a77c19a07df2f666ea36f7879462e36fc76959f60cd29ac96341c4ffffffb
 /// 6350874878119819312338956282401532410528162663560392320966563075034087161851
-/// 
+///
 // const R: Fr = Fr([
 //     0xac96341c4ffffffb,
 //     12436184717236109307
-// 
+//
 //     0x36fc76959f60cd29,
 //     3962172157175319849
-// 
+//
 //     0x666ea36f7879462e,
 //     7381016538464732718
-// 
+//
 //     0x0e0a77c19a07df2f,
 //     1011752739694698287
 // ]);
 
 fn main() {
-	let v = BigUint::from_str("944936681149208446651664254269745548490766851729442924617792859073125903783").unwrap();
-	let res = biguint_to_real_u64_vec(v, 4);
-	println!("{:?}", res);
+    let v = BigUint::from_str(
+        "16950150798460657717958625567821834550301663161624707787222815936182638968203",
+    )
+    .unwrap();
+    let res = biguint_to_real_u64_vec(v, 4);
+    println!("{:?}", res);
 }
 
 /// Convert BigUint into a vector of 64-bit limbs.
